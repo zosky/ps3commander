d22fb4a74ba97fe4c096d5e837e1e4a1c64e3bb5
marc bulbulian <boolioncube@gmail.com>
2021-08-22 23:36:11 -0400

    svgIcons (move/guitar) + family (mdl-map)

---
+++ src/components/family.vue
@@ -0,0 +1,15 @@
+<template>
+  <AccountChild v-if="u == 'ben'" />
+  <Face v-else-if="u == 'mel'" />
+  <Memory v-else-if="u == 'marc'" />
+  <HomeModern v-else-if="u == 'family'" />
+</template>
+
+<script>
+import { AccountChild, HomeModern, Face, Memory } from "mdue";
+export default {
+  name: "familySVGs",
+  props: { u: { type: String, default: "marc" } },
+  components: { AccountChild, HomeModern, Face, Memory },
+};
+</script>
