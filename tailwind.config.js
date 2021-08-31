38a77e1893610f7676635508f3aa777087e8c917
marc bulbulian <boolioncube@gmail.com>
2021-08-30 22:44:04 -0400

    proper tailWind safeWorks syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 module.exports = {
   purge: {
     content: ["./public/**/*.html", "./src/**/*.vue"],
-    safelist: [
-      "grid-cols-1",
-      "grid-cols-2",
-      "grid-cols-3",
-      "grid-cols-4",
-      "grid-cols-5",
-      "grid-cols-6",
-      "sm:grid-cols-1",
-      "sm:grid-cols-2",
-      "sm:grid-cols-3",
-      "sm:grid-cols-4",
-      "sm:grid-cols-5",
-      "sm:grid-cols-6",
-    ],
+    options: {
+      safelist: [
+        "grid-cols-1",
+        "grid-cols-2",
+        "grid-cols-3",
+        "grid-cols-4",
+        "grid-cols-5",
+        "grid-cols-6",
+        "sm:grid-cols-1",
+        "sm:grid-cols-2",
+        "sm:grid-cols-3",
+        "sm:grid-cols-4",
+        "sm:grid-cols-5",
+        "sm:grid-cols-6",
+      ],
+    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
