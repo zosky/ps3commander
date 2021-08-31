ed9b0329e519b4ca4907797ebedaff14e825a507
marc bulbulian <boolioncube@gmail.com>
2021-08-30 22:35:45 -0400

    force tailwindsafe list - all col sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  purge: {
+    content: ["./public/**/*.html", "./src/**/*.vue"],
+    safelist: [
+      "grid-cols-1",
+      "grid-cols-2",
+      "grid-cols-3",
+      "grid-cols-4",
+      "grid-cols-5",
+      "grid-cols-6",
+      "sm:grid-cols-1",
+      "sm:grid-cols-2",
+      "sm:grid-cols-3",
+      "sm:grid-cols-4",
+      "sm:grid-cols-5",
+      "sm:grid-cols-6",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
